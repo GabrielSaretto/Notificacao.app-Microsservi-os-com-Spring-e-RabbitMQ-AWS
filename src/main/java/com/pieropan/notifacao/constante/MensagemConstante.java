5cff489d86e2de04beff3031b8da84b079e4a923
GabrielSaretto <gabriel.saretto2@gmail.com>
2024-08-20 22:25:36 -0300

    Crianto configuracao do Amazon SNS

---
+++ src/main/java/com/pieropan/notifacao/constante/MensagemConstante.java
@@ -0,0 +1,6 @@
+package com.pieropan.notifacao.constante;
+
+public class MensagemConstante {
+
+    public static final String PROPOSTA_EM_ANALISE = "Prezado(a) %s, sua proposta foi recebida por nossa equipe e sera analisada. Em breve retornaremos o contato";
+}
--- src/main/java/com/pieropan/notifacao/listener/PropostaPendenteListener.java
+++ src/main/java/com/pieropan/notifacao/listener/PropostaPendenteListener.java
@@ -1,13 +1,18 @@
 package com.pieropan.notifacao.listener;
 
+import com.pieropan.notifacao.constante.MensagemConstante;
+import com.pieropan.notifacao.domain.Proposta;
+import com.pieropan.notifacao.service.NotificacaoSnsService;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
 @Component
 public class PropostaPendenteListener {
 
-    @RabbitListener(queues = "${rabbitmq.queue.proposta.pendente}")
-    public void propostaPendente(){
+    private NotificacaoSnsService notificacaoSnsService;
 
+    @RabbitListener(queues = "${rabbitmq.queue.proposta.pendente}")
+    public void propostaPendente(Proposta proposta){
+        String mensagem = String.format(MensagemConstante.PROPOSTA_EM_ANALISE, proposta.getUsuario().getNome());
     }
 }
